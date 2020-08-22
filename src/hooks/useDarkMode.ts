afa48a8fd0f399d5d6a29afde9744febde2d8112
Jinwoo Jeong <thinkratomos@gmail.com>
2020-08-22 17:00:58 +0900

    SSR 을 위해 window 타입체크

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,9 +1,11 @@
 // Ext
 import { useEffect, useState, useCallback } from 'react'
 
-const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)")
+const darkModeQuery = typeof window !== 'undefined' && window.matchMedia("(prefers-color-scheme: dark)")
 
 function useDarkMode() {
+  if (!darkModeQuery) { return }
+
   const [isDarkMode, setIsDarkMode] = useState(darkModeQuery.matches)
 
   const handleChangeDarkMode = useCallback((e: MediaQueryListEvent) => {

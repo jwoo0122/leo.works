7c38f1ddc3d610d852d3ba346bd70a67154d48cd
Jinwoo Jeong <thinkratomos@gmail.com>
2020-08-22 23:00:38 +0900

    SEO 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,7 @@ module.exports = {
                     path
                     title
                     date
+                    description
                   }
                 }
               }

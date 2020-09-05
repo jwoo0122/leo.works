b58c374596c638c0c56fb8069d3f2c60e09867b7
Jinwoo Jeong <thinkratomos@gmail.com>
2020-09-05 22:09:25 +0900

    알고리아에 featured image 업로드

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,6 +90,7 @@ module.exports = {
                     title
                     date
                     description
+                    featuredImage
                   }
                 }
               }

9e67adfaa6f635eaa5a4d081d6bc52fdd551c2da
Jinwoo Jeong <thinkratomos@gmail.com>
2020-12-13 23:57:42 +0900

    Add background pattern

--- src/components/IndexTitle/IndexTitle.tsx
+++ src/components/IndexTitle/IndexTitle.tsx
@@ -1,27 +1,14 @@
-// Ext
-import { useCallback, useState } from 'react'
-import classNames from 'classnames'
-
 // Int
 import { profileUrl } from 'Constants/Gravatar'
 import styles from './IndexTitle.module.scss'
 
 function IndexTitle() {
-  const [hasError, setHasError] = useState(false)
-
-  const handleError = useCallback(() => {
-    setHasError(true)
-  }, [])
-
   return (
     <div className={styles.titleContainer}>
-      <div className={classNames(styles.profileContainer, {
-        [styles.error]: hasError
-      })}>
+      <div className={styles.profileContainer}>
         <img
           className={styles.profileImage}
           src={profileUrl}
-          onError={handleError}
         />
       </div>
       <div className={styles.titleBy}>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,20 +2,16 @@ import Header from "Components/Header"
 import styles from "./Layout.module.scss"
 
 interface LayoutProps extends React.HTMLProps<HTMLDivElement> {
-  hideHeaderLine?: boolean
   children: React.ReactNode
 }
 
 export default function Layout({
-  hideHeaderLine = false,
   children,
   ...otherProps
 }: LayoutProps) {
   return (
     <div className={styles.ancient}>
-      { !hideHeaderLine && (
-        <Header />
-      ) }
+      <Header />
       <div className={styles.rootWithBackground}>
         <div className={styles.rootContainer}>
           <div {...otherProps}>{children}</div>

0a46a50d7bf06d92a998d317be9eb8306e107ac9
Jinwoo Jeong <thinkratomos@gmail.com>
2023-02-07 02:03:06 +0900

    feat: title decoration

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "leo.works";
-export const SITE_DESCRIPTION = "하학상달";
+export const SITE_TITLE = "下學上達";
+export const SITE_DESCRIPTION = "낮고 쉬운 것을 배워 깊고 어려운 이치를 깨달음";

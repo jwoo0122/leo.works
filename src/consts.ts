c57e9095707cca780971a6b231ade082e8a25ee8
Jinwoo Jeong <thinkratomos@gmail.com>
2023-02-08 01:14:05 +0900

    feat: change header design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "下學上達";
+export const SITE_TITLE_FIRST = "下學";
+export const SITE_TITLE_SECOND = "上達";
+export const SITE_TITLE = SITE_TITLE_FIRST + SITE_TITLE_SECOND;
 export const SITE_DESCRIPTION = "낮고 쉬운 것을 배워 깊고 어려운 이치를 깨달음";

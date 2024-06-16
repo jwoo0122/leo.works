136135548d3d223ef41b2ae842ab2c7d2092e619
Jinwoo Jeong <thinkratomos@gmail.com>
2024-06-16 22:18:17 +0900

    chore: change yarn to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from 'astro-icon'
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://leo.works",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
   markdown: {
     shikiConfig: {
       theme: "github-dark",
@@ -16,6 +17,9 @@ export default defineConfig({
   },
   // FIXME: https://github.com/natemoo-re/astro-icon/issues/35
   vite: {
+    ssr: {
+      external: ['svgo']
+    },
     resolve: {
       alias: {
         svgo: import.meta.env.PROD ? "svgo/dist/svgo.browser.js" : "svgo",

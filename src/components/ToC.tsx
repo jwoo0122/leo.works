0c9cc7adce28555371f02d6107ad3709fdede5a8
Jinwoo Jeong <thinkratomos@gmail.com>
2024-06-28 14:58:24 +0900

    style

--- src/components/ToC.tsx
+++ src/components/ToC.tsx
@@ -1,47 +1,41 @@
 import type { MarkdownHeading } from "astro";
+import { useState } from "react";
 
 interface ToCProps {
   headings: MarkdownHeading[];
 }
+
+const downchevron = `-translate-y-0.5 rotate-45`
+const upchevron = `translate-y-0.5 -rotate-[135deg]`
 
 export function ToC({ headings }: ToCProps) {
   if (headings.length == 0) {
     return null;
   }
 
+  const [isOpened, toggle] = useState(false)
+
   return (
-    <>
-      <div
-        style={{
-          fontSize: 18,
-          fontWeight: "bold",
-          marginBottom: 8,
-          marginTop: 32,
-        }}
-      >
+    <div className="my-3 cursor-pointer rounded-md bg-slate-200 px-4 py-2 dark:bg-slate-800" onClick={() => toggle(prev => !prev)}>
+      <div className="flex items-center justify-between font-bold">
         Contents
+        <div className={`ml-2 size-2 ${isOpened ? upchevron : downchevron} border-b-2 border-r-2 border-slate-500`}></div>
       </div>
-      <div
-        style={{
-          fontSize: 14,
-        }}
-      >
-        {headings.map((h) => (
-          <div
-            style={{
-              marginLeft: 24 * (h.depth - 1),
-              marginBottom: 8,
-            }}
-          >
-            <a
+      {isOpened && (
+        <div className="mt-4">
+          {headings.map((h) => (
+            <a className="mt-1 block underline"
+              style={{
+                marginLeft: 24 * (h.depth - 1),
+              }}
               href={`#${h.slug}`}
-              style={{ color: "rgba(120, 120, 120, 0.8)" }}
+              onClick={e => e.stopPropagation()}
             >
               {h.text}
             </a>
-          </div>
-        ))}
-      </div>
-    </>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/forMdx/Quote.tsx
+++ src/components/forMdx/Quote.tsx
@@ -2,33 +2,21 @@ interface QuoteProps extends React.PropsWithChildren {
   style: "default" | "disclaimer";
 }
 
+const COLORS: { [key in QuoteProps['style']]: { background: string; bar: string; } } = {
+  'default': {
+    background: 'bg-slate-200 dark:bg-slate-700',
+    bar: 'bg-slate-400 dark:bg-slate-300'
+  },
+  'disclaimer': {
+    background: 'bg-orange-200 dark:bg-orange-900',
+    bar: 'bg-orange-500 dark:bg-orange-500'
+  }
+}
+
 export default function Quote({ style = "default", children }: QuoteProps) {
-  const backgroundColor =
-    style === "disclaimer" ? "rgba(230, 200, 0, 0.2)" : "rgba(0, 0, 0, 0.1)";
-
-  const sidebarBackgroundColor =
-    style === "disclaimer" ? "rgb(230, 200, 0)" : "grey";
-
   return (
-    <div
-      style={{
-        position: "relative",
-        padding: "1px 24px 1px 18px",
-        margin: "4px 0",
-        fontStyle: "italic",
-        backgroundColor,
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          left: 0,
-          top: 0,
-          height: "100%",
-          width: 3,
-          backgroundColor: sidebarBackgroundColor,
-        }}
-      />
+    <div className={`relative my-2 px-6 py-1 text-sm ${COLORS[style].background}`}>
+      <div className={`absolute left-0 top-0 h-full w-1 ${COLORS[style].bar}`} />
       {children}
     </div>
   );

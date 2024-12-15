4335c75178147c2778856289fbd9fc5c36a985c2
Jinwoo Jeong <thinkratomos@gmail.com>
2024-12-16 00:17:41 +0900

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE_FIRST = "下學";
-export const SITE_TITLE_SECOND = "上達";
-export const SITE_TITLE = SITE_TITLE_FIRST + SITE_TITLE_SECOND;
-export const SITE_DESCRIPTION = "낮고 쉬운 것을 배워 깊고 어려운 이치를 깨달음";
+export const SITE_TITLE = 'leo.works';
+export const SITE_DESCRIPTION = '';
